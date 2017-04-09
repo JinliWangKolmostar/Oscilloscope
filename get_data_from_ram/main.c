b6b32d4540f342fac0e6dab5e05d1a12af1a7715
JinliWangKolmostar <jingli.wang@kolmostar.com>
2017-04-09 22:01:44 +0800

    osciloscope continuous collection, but don't compile

--- get_data_from_ram/main.c
+++ get_data_from_ram/main.c
@@ -7,7 +7,7 @@
 
 static const unsigned int MEMORY_DEPTH = 120000;
 static const unsigned int SINGLE_READ_MAX_LEN = 250000;
-static const char origin_file_name[64] = "C:\\rigol_data\\data.bin";
+static const unsigned int RIGOL_HEAD_MAX_LEN = 11;
 static const char source_file_name[64] = "C:\\rigol_data\\valid_data.bin";
 static const unsigned char data_mask = 0x0F;
 
@@ -35,7 +35,7 @@ void DataFormatConverse()
 {
     __int64_t data_wrap = 0;
 	size_t data_size;
-	unsigned char *read_buffer = (unsigned char *)malloc(SINGLE_READ_MAX_LEN * sizeof(char));
+	unsigned char *read_buffer = (unsigned char *)malloc((SINGLE_READ_MAX_LEN + RIGOL_HEAD_MAX_LEN) * sizeof(char));
 
 	FILE *fp_source = fopen(origin_file_name, "rb");
 	FILE *fp_destin = fopen(source_file_name, "wb");
@@ -93,7 +93,6 @@ int DiscardDataHead(unsigned char *buffer)
 
 int main(int argc, char* argv[])
 {
-#if 0
 	ViSession defaultRM, vi;
 
 	char strStarPos[128];
@@ -121,39 +120,45 @@ int main(int argc, char* argv[])
 	viFindRsrc(defaultRM, "USB?*", &list,&nmatches, matches);
 	viOpen (defaultRM,matches,VI_NULL,VI_NULL,&vi);
 
-	//stop
-	viWrite(vi, (unsigned char *)strStop, strlen(strStop), &strStop_WriteLen);
 	//set source
 	viWrite(vi, (unsigned char *)strSource, strlen(strSource), &strSource_WriteLen);
 	//set
 	viWrite(vi, (unsigned char *)strMode, strlen(strMode), &strMode_WriteLen);
 
-	FILE *fp = fopen(origin_file_name, "wb");
-	int i;
-	for(i = 0; i < 2; i++) {
-		sprintf(strStarPos, ":WAV:STAR %u\n", i * SINGLE_READ_MAX_LEN + 400001);
-		sprintf(strStopPos, ":WAV:STOP %u\n", (i + 1) * SINGLE_READ_MAX_LEN + 400001);
-		//set star position
-		viWrite(vi, (unsigned char *)strStarPos, strlen(strStarPos), &strStarPos_WriteLen);
-		//set end position
-		viWrite(vi, (unsigned char *)strStopPos, strlen(strStopPos), &strStopPos_WriteLen);
-		//send get data commend
-		viWrite(vi, (unsigned char *)strGetData, strlen(strGetData), &strGetData_WriteLen);
-		//read data from ram of oscilloscope
-		viRead(vi, buffer, SINGLE_READ_MAX_LEN, &readLen);
+	int file_num = 0;
+    char des_file_name[64];
+	while(1)
+	{
+		//stop
+		viWrite(vi, (unsigned char *)strStop, strlen(strStop), &strStop_WriteLen);
 
-		int offset = DiscardDataHead(buffer);
+        sprintf(des_file_name, "C:\\rigol_data\\rigol_data_%d.bin", file_num++);
+		FILE *fp = fopen(des_file_name, "wb");
+		int i;
+		for(i = 0; i < 2; i++) {
+			sprintf(strStarPos, ":WAV:STAR %u\n", i * SINGLE_READ_MAX_LEN + 400001);
+			sprintf(strStopPos, ":WAV:STOP %u\n", (i + 1) * SINGLE_READ_MAX_LEN + 400001);
+			//set star position
+			viWrite(vi, (unsigned char *)strStarPos, strlen(strStarPos), &strStarPos_WriteLen);
+			//set end position
+			viWrite(vi, (unsigned char *)strStopPos, strlen(strStopPos), &strStopPos_WriteLen);
+			//send get data commend
+			viWrite(vi, (unsigned char *)strGetData, strlen(strGetData), &strGetData_WriteLen);
+			//read data from ram of oscilloscope
+			viRead(vi, buffer, SINGLE_READ_MAX_LEN, &readLen);
 
-		fwrite(buffer + offset, 1, readLen - offset, fp);
+			fwrite(buffer + RIGOL_HEAD_MAX_LEN, 1, readLens, fp);
+		}
+		viWrite(vi, (unsigned char *)strRun, strlen(strRun), &strStop_WriteLen);
+
+		fclose(fp);
 	}
-	viWrite(vi, (unsigned char *)strRun, strlen(strRun), &strStop_WriteLen);
 
-	fclose(fp);
 	viClose (vi);
 	viClose (defaultRM);
 	free(buffer);
 	free(matches);
-#endif
+
     //DataFormatConverse();
     //find_substring();
     //test_GetValidData();
